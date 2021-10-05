11be0e521e34a0c386fe1d59cbfea8255ce81f24
Felice Pantaleo <felice.pantaleo@cern.ch>
2021-10-05 11:56:06 +0200

    move reduction to int to avoid non associativity of floating point

--- hands-on/cuda_exercises/ex05/reduction.cu
+++ hands-on/cuda_exercises/ex05/reduction.cu
@@ -1,26 +1,20 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <vector>
 #include <numeric>
 #include <iostream>
+#include <random>
+
 
 // Here you can set the device ID that was assigned to you
 #define MYDEVICE 0
 
-double random_double(void)
-{
-
-  return static_cast<double>(rand()) / RAND_MAX;
-}
-
 
 // Part 1 of 6: implement the kernel
-__global__ void block_sum(const double *input,
-                          double *per_block_results,
+__global__ void block_sum(const int *input,
+                          int *per_block_results,
                           const size_t n)
 {
   //fill me
-  __shared__ double sdata[];
+  __shared__ int sdata[10];
 
 
 
@@ -31,37 +25,40 @@ __global__ void block_sum(const double *input,
 ////////////////////////////////////////////////////////////////////////////////
 int main(void)
 {
+  
+  std::random_device rd;  //Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+  std::uniform_int_distribution<> distrib(-10, 10);
   // create array of 256ki elements
   const int num_elements = 1<<18;
-  srand(time(NULL));
   // generate random input on the host
-  std::vector<double> h_input(num_elements);
-  for(int i = 0; i < h_input.size(); ++i)
+  std::vector<int> h_input(num_elements);
+  for(auto& elt: h_input)
   {
-    h_input[i] = random_double();
+    elt = distrib(gen);
   }
 
-  const double host_result = std::accumulate(h_input.begin(), h_input.end(), 0.0f);
+  const int host_result = std::accumulate(h_input.begin(), h_input.end(), 0);
   std::cerr << "Host sum: " << host_result << std::endl;
 
-  //Part 1 of 6: move input to device memory
-  double *d_input = 0;
+  // //Part 1 of 6: move input to device memory
+  int *d_input;
 
-  // Part 1 of 6: allocate the partial sums: How much space does it need?
-  double *d_partial_sums_and_total = 0;
+  // // Part 1 of 6: allocate the partial sums: How much space does it need?
+  int *d_partial_sums_and_total ;
 
-  // Part 1 of 6: launch one kernel to compute, per-block, a partial sum. How much shared memory does it need?
+  // // Part 1 of 6: launch one kernel to compute, per-block, a partial sum. How much shared memory does it need?
   block_sum<<<num_blocks,block_size>>>(d_input, d_partial_sums_and_total, num_elements);
 
-  // Part 1 of 6: compute the sum of the partial sums
+  // // Part 1 of 6: compute the sum of the partial sums
   block_sum<<<>>>();
 
-  // Part 1 of 6: copy the result back to the host
-  double device_result = 0;
+  // // Part 1 of 6: copy the result back to the host
+  int device_result = 0;
 
-  std::cout << "Device sum: " << device_result << std::endl;
+  // std::cout << "Device sum: " << device_result << std::endl;
 
-  // Part 1 of 6: deallocate device memory
+  // // Part 1 of 6: deallocate device memory
 
 
   return 0;
