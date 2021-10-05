2532c467e6de6cc6a6d7e634f173d5257f225c7d
Francesco Giacomini <francesco.giacomini@cnaf.infn.it>
2021-10-05 12:21:06 +0200

    Update move/rvo exercises

--- hands-on/cpp/rvo.cpp
+++ hands-on/cpp/rvo.cpp
@@ -17,14 +17,13 @@ std::vector<int> make_vector_rvo(int N, std::function<int()> gen)
 }
 
 void make_vector_out(std::vector<int>& result, int N, std::function<int()> gen);
-std::vector<int> make_vector_norvo(int N, std::function<int()> gen);
 
 int main(int argc, char* argv[])
 {
   int const N = (argc > 1) ? std::atoi(argv[1]) : 1000000;
 
   std::random_device rd;
-  std::mt19937 eng{rd()};
+  std::default_random_engine eng{rd()};
 
   int const MAX_N = 100;
   std::uniform_int_distribution<int> dist{1, MAX_N};
@@ -32,15 +31,13 @@ int main(int argc, char* argv[])
   std::function<int()> gen = [&]{ return dist(eng); };
 
   {
-    auto t0 = std::chrono::high_resolution_clock::now();
+    auto t0 = std::chrono::steady_clock::now();
     std::vector<int> v = make_vector_rvo(N, gen);
-    auto t1 = std::chrono::high_resolution_clock::now();
+    auto t1 = std::chrono::steady_clock::now();
     std::cout << "rvo: " << N << " random ints in "
               << std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count()
               << " ms\n";
   }
-
-  // std::vector<int> v = make_vector_norvo(N, gen);
 
   // std::vector<int> v;
   // make_vector_out(v, N, gen);
