3abf46600ca65d506bd6533e54334655c32f4536
Felice Pantaleo <felice.pantaleo@cern.ch>
2021-10-05 12:12:53 +0200

    update

--- hands-on/architecture/branchPredictor.cpp
+++ hands-on/architecture/branchPredictor.cpp
@@ -3,36 +3,35 @@
 // try with -O2 -Ofast and then
 // c++ -Ofast -march=native branchPredictor.cpp -funroll-all-loops
 #include <algorithm>
-#include <vector>
 #include <iostream>
+#include <vector>
 
-int main( int argc, char**)
+int main(int argc, char**)
 {
-    // generate data
-    const size_t arraySize = 32768;
-    std::vector<int> test(arraySize);
-    std::vector<int> data(arraySize);
+  // generate data
+  const size_t arraySize = 32768;
+  std::vector<int> test(arraySize);
+  std::vector<int> data(arraySize);
 
+  for (unsigned c = 0; c < arraySize; ++c) {
+    test[c] = std::rand() % 256;
+    data[c] = std::rand() % 256;
+  }
+
+  // If the data are sorted like shown here the program runs about
+  // 6x faster (on my test machine, with -O2)
+  if (argc > 1)
+    std::sort(test.begin(), test.end());
+
+  long long sum = 0;
+
+  for (unsigned i = 0; i < 100000; ++i) {
     for (unsigned c = 0; c < arraySize; ++c) {
-        test[c] = std::rand() % 256;
-        data[c] = std::rand() % 256;
+      if (test[c] >= 128)
+        sum += data[c];
     }
+  }
+  std::cout << "sum = " << sum << ' ' << argc << std::endl;
 
-    // If the data are sorted like shown here the program runs about
-   // 6x faster (on my test machine, with -O2)
-   if (argc>1)  std::sort(test.begin(), test.end());
-
-    long long sum = 0;
-
-    for (unsigned i = 0; i < 100000; ++i)
-    {
-        for (unsigned c = 0; c < arraySize; ++c)
-        {
-            if (test[c] >= 128)
-                sum += data[c];
-        }
-    }
-    std::cout << "sum = " << sum << ' ' << argc << std::endl;
-
-    return 0;
+  return 0;
 }
